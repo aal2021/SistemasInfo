5e80eb1ea243f697fe3c15f9600cf0c362dbc8d4
aguadoeduardo <e.aguado.2021@alumnos.urjc.es>
2024-03-16 20:10:37 +0100

    Ej4_2 con Gráfico y Error Solucionado

--- Práctica1_SI/Ejercicio4/Ej4_2.py
+++ Práctica1_SI/Ejercicio4/Ej4_2.py
@@ -8,7 +8,7 @@ conn = sqlite3.connect('datos.db')
 # Leer los datos relevantes de la base de datos
 # Seleccionamos los el id_usuario, el si la contraseña es segura o no y la probabilidad de
 # que un usuario clique en un email de phishing de todos los que recibe
-df = pd.read_sql_query('''SELECT u.id_usuario, u.contrasena_segura, (CAST(e.cliclados as float)/e.total)*100 as probabilidad_spam
+df = pd.read_sql_query('''SELECT u.nombre_usuario,u.id_usuario, u.contrasena_segura, (CAST(e.cliclados as float)/e.phishing)*100 as probabilidad_spam
                           FROM usuarios u
                           INNER JOIN emails e ON u.id_usuario = e.id_usuario
                           ''', conn)
@@ -18,4 +18,14 @@ usuarios_debiles = df[df['contrasena_segura']==0]
 
 # Seleccionar los 10 usuarios más críticos (con contraseñas débiles y alta probabilidad de spam)
 usuarios_criticos = usuarios_debiles.nlargest(10, 'probabilidad_spam')
-print(usuarios_criticos)
+#print(usuarios_criticos)
+
+# Graficar los resultados
+plt.figure(figsize=(10, 6))
+usuarios_criticos.set_index('nombre_usuario')['probabilidad_spam'].plot(kind='bar', color='skyblue')
+plt.title('Los 10 Usuarios Más Críticos')
+plt.xlabel('Nombre de Usuario')
+plt.ylabel('Probabilidad de Hacer Clic en Spam')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
