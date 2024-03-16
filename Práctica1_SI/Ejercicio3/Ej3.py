6bb9b59ae2cf964420f091eef23f3feaaef1f692
a.lopezg.2021 <a.lopezg.2021@alumnos.urjc.es>
2024-03-16 18:09:17 +0100

    Ejercicio 3 Parte 1

---
+++ Práctica1_SI/Ejercicio3/Ej3.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import sqlite3
+
+# Conectarse a la base de datos
+conn = sqlite3.connect('datos.db')
+
+
+
+df = pd.read_sql_query('''SELECT *
+                                   FROM usuarios u INNER JOIN emails e ON e.id_usuario=u.id_usuario
+                                   ''', conn)
+
+df_perm = df.groupby("permisos")
+df_perm_0 = df_perm.get_group(0)
+df_perm_1 = df_perm.get_group(1)
+
+# Calcular la información solicitada para la variable dentro del email de phishing para permisos igual a 0
+print("Para permisos igual a 0:")
+print("Número de observaciones:", len(df_perm_0))
+print("Número de valores ausentes (missing):", df_perm_0['phishing'].isnull().sum())
+print("Mediana:", df_perm_0['phishing'].median())
+print("Media:", df_perm_0['phishing'].mean())
+print("Varianza:", df_perm_0['phishing'].var())
+print("Valor máximo:", df_perm_0['phishing'].max())
+print("Valor mínimo:", df_perm_0['phishing'].min())
+
+# Calcular la información solicitada para la variable dentro del email de phishing para permisos igual a 1
+print("\nPara permisos igual a 1:")
+print("Número de observaciones:", len(df_perm_1))
+print("Número de valores ausentes (missing):", df_perm_1['phishing'].isnull().sum())
+print("Mediana:", df_perm_1['phishing'].median())
+print("Media:", df_perm_1['phishing'].mean())
+print("Varianza:", df_perm_1['phishing'].var())
+print("Valor máximo:", df_perm_1['phishing'].max())
+print("Valor mínimo:", df_perm_1['phishing'].min())
