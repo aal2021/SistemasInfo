68d0b7da7bc3d3ae7740abd8b36f98dec97b73df
a.lopezg.2021 <a.lopezg.2021@alumnos.urjc.es>
2024-03-16 21:19:35 +0100

    Ejercicio 3 Parte 2

--- Práctica1_SI/Ejercicio3/Ej3.py
+++ Práctica1_SI/Ejercicio3/Ej3.py
@@ -33,3 +33,29 @@ print("Media:", df_perm_1['phishing'].mean())
 print("Varianza:", df_perm_1['phishing'].var())
 print("Valor máximo:", df_perm_1['phishing'].max())
 print("Valor mínimo:", df_perm_1['phishing'].min())
+
+print("\n-------------------------------------------------------------------------------------")
+
+df_pass = df.groupby("contrasena_segura")
+df_pass_0 = df_pass.get_group(0)
+df_pass_1 = df_pass.get_group(1)
+
+# Calcular la información solicitada para la variable dentro del email de phishing para permisos igual a 0
+print("\nPara contraseñas no seguras:")
+print("Número de observaciones:", len(df_pass_0))
+print("Número de valores ausentes (missing):", df_pass_0['phishing'].isnull().sum())
+print("Mediana:", df_pass_0['phishing'].median())
+print("Media:", df_pass_0['phishing'].mean())
+print("Varianza:", df_pass_0['phishing'].var())
+print("Valor máximo:", df_pass_0['phishing'].max())
+print("Valor mínimo:", df_pass_0['phishing'].min())
+
+# Calcular la información solicitada para la variable dentro del email de phishing para permisos igual a 1
+print("\nPara contraseñas seguras:")
+print("Número de observaciones:", len(df_pass_1))
+print("Número de valores ausentes (missing):", df_pass_1['phishing'].isnull().sum())
+print("Mediana:", df_pass_1['phishing'].median())
+print("Media:", df_pass_1['phishing'].mean())
+print("Varianza:", df_pass_1['phishing'].var())
+print("Valor máximo:", df_pass_1['phishing'].max())
+print("Valor mínimo:", df_pass_1['phishing'].min())
