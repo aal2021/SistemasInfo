443ddecb959638dd9b36374b04901213a5ea9ad0
aal2021 <a.alcazar.2021@alumnos.urjc.es>
2024-03-17 15:14:15 +0100

    Flask EJ2 EJ3

--- Práctica1_SI/app.py
+++ Práctica1_SI/app.py
@@ -1,12 +1,31 @@
-from flask import Flask
+import io
+
+from flask import Flask, send_file, Response
+import subprocess
+from flask import render_template
+from flask import request
+import json
+import jsonify
+import plotly.graph_objects as go
+from matplotlib import pyplot as plt
 
 app = Flask(__name__)
 
+@app.route('/EJ2')
+def root2():
+    output = subprocess.check_output(['python', 'Ejercicio2/Consultas.py'])
+    output2 = output.decode("latin1")
+    output3 = output2.replace('\n', '<br>')
+    return output3
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/EJ3')
+def root3():
+    output = subprocess.check_output(['python', 'Ejercicio3/EJ3.py'])
+    output2 = output.decode("latin1")
+    output3 = output2.replace('\n', '<br>')
+    return output3
+
 
 
 if __name__ == '__main__':
-    app.run()
+   app.run(debug = True)
