b5535fc96449fec2d80596c814e04b16a83d668c
GonzaloPC <g.polonio.2021@alumnos.urjc.es>
2024-04-16 19:24:48 +0200

    Ejercicio4_Practica2 (Añadir lo que faltaba)

--- Práctica1_SI/app.py
+++ Práctica1_SI/app.py
@@ -1,10 +1,13 @@
-from flask import Flask, send_file, Response
 import subprocess
+from flask import Flask, render_template, request, make_response, send_file
+import io
+from reportlab.pdfgen import canvas
 
 from Ejercicio4.Ej4_1 import generar_grafico1
 from Ejercicio4.Ej4_2 import generar_grafico2
 from Ejercicio4.Ej4_3 import generar_grafico3
 from Ejercicio4.Ej4_4 import generar_grafico4
+from Práctica1_SI.Ejercicio4_Practica2.GeneradorPDFs import usuariosCriticos
 
 app = Flask(__name__)
 
@@ -41,6 +44,35 @@ def root4_3():
 def root4_4():
     img_bytes = generar_grafico4()
     return send_file(img_bytes, mimetype='image/png')
+
+@app.route('/GeneradorPDF', methods=['GET', 'POST'])
+def generar_pdf():
+    if request.method == 'POST':
+        # Obtener el número de usuarios a incluir en el informe desde el formulario
+        num_elementos = int(request.form['num_elementos'])
+
+        # Generar informe de usuarios críticos
+        informe_usuarios = usuariosCriticos(num_elementos)
+
+        # Crear un nuevo PDF
+        buffer = io.BytesIO()
+        c = canvas.Canvas(buffer)
+
+        # Agregar contenido al PDF
+        c.drawString(100, 750, "Informe de Usuarios Críticos:")
+        c.drawString(100, 700, informe_usuarios)
+
+        c.save()
+
+        # Construir la respuesta con el PDF generado
+        buffer.seek(0)
+        response = make_response(buffer.getvalue())
+        response.headers['Content-Type'] = 'application/pdf'
+        response.headers['Content-Disposition'] = 'attachment; filename=informe.pdf'
+
+        return response
+
+    return render_template('generador_pdf.html')
 
 if __name__ == '__main__':
    app.run(debug = True)
